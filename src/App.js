cfe9266508169a9c55126ca59618c25663da3136
Othman Alomair <most3mr@live.com>
2022-02-06 13:39:04 +0300

    day 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
+import { Link } from "react-router-dom"
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
+import FeedbackStats from "./components/FeedbackStats"
 import FeedbackData from "./data/feedbackData"
+import FeedbackForm from "./components/FeedbackForm"
+import AboutPage from "./pages/AboutPage"
+
+// Tommorow check this link https://reactrouter.com/docs/en/v6/getting-started/tutorial#add-some-links and try to finsh the router
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    const addFeedback = (newFeedback) => {
+        setFeedback(prevFeedback => [{...newFeedback, id: prevFeedback.length + 1}, ...prevFeedback])
+    }
+
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure about that')) {
+            setFeedback(prevFeedback => prevFeedback.filter(item => item.id !== id))
+        }
+    }
 
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackList feedback={feedback} />
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList handleDelete={deleteFeedback} feedback={feedback} />
+                <AboutPage />
             </div>
         </>
     )
--- src/components/Feedbackitem.jsx
+++ src/components/Feedbackitem.jsx
@@ -1,11 +1,22 @@
-function Feedbackitem({item}) {
+import { FaTimes } from "react-icons/fa"
+import Card from "./shared/Card"
+import PropTypes from "prop-types"
 
+function Feedbackitem({item,handleDelete}) {
   return (
-    <div className="card">
+    <Card>
         <div className="num-display">{item.rating}</div>
+        <button onClick={() => handleDelete(item.id)} className="close">
+          <FaTimes color="purple" />
+        </button>
         <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   )
 }
+
+Feedbackitem.propTypes = {
+  item: PropTypes.object.isRequired
+}
+
 
 export default Feedbackitem
